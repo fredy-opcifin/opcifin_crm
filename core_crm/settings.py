8a968d6968d00656f9642151d9412a4055b47b0e
Fredy Díaz <fredydmartinez145@gmail.com>
2026-06-24 23:08:05 -0500

    Update settings.py

--- core_crm/settings.py
+++ core_crm/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%nom1n)o$vh+ff*9m-h17+#zk$8b-h&w*x6(_@%-4=qos_#g)-'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,7 +135,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'atencionalclienteopcifin@gmail.com'
-EMAIL_HOST_PASSWORD = 'meylrazyduuevrld'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = 'OPCIFIN <atencionalclienteopcifin@gmail.com>'
 
 # Acceso al Dashboard Moderno
