1f486fe39c3ba4ac94839b6f85438a0cff4fcf3d
Fredy Díaz <fredydmartinez145@gmail.com>
2026-06-24 23:15:16 -0500

    Update settings.py

--- core_crm/settings.py
+++ core_crm/settings.py
@@ -134,9 +134,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'atencionalclienteopcifin@gmail.com'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = 'OPCIFIN <atencionalclienteopcifin@gmail.com>'
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 # Acceso al Dashboard Moderno
 
